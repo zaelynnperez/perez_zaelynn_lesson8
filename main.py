ed936117f06cb3c88b7e8ebc21aa2fa31261907e
zaelynnperez <72786696+zaelynnperez@users.noreply.github.com>
2021-02-08 21:09:14 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 while True:
     print( input.light_level())
+    if input.light_level() >1:
+        light.set_all(light.rgb(255, 255, 255))
--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
 while (true) {
     console.log(input.lightLevel())
+    if (input.lightLevel() > 1) {
+        light.setAll(light.rgb(255, 255, 255))
+    }
+    
 }
